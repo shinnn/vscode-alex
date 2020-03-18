2c1c40341659f954e6e24aa0c92e25c33332da7f
tlahmann <tobiaslahmann@gmail.com>
2020-03-18 11:25:43 +0100

    Enable markdown and tex files

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -30,7 +30,11 @@ export function activate(context: ExtensionContext) {
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+        documentSelector: [
+            { scheme: 'file', language: 'plaintext' },
+            { scheme: 'file', language: 'markdown' },
+            { scheme: 'file', language: 'latex' }
+        ],
         synchronize: {
             // Notify the server about file changes to '.clientrc files contained in the workspace
             fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
